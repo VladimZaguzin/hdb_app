708ef37225130ba2e98c1788f77487d871480195
Vladimir Zaguzin <vladim160@gmail.com>
2017-12-06 21:25:13 -0500

    123

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
+const path = require('path')
+const port = process.env.PORT || 8080 ;
 var app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static('./node_modules/bootstrap/dist'));
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
 
-var server = app.listen(process.env.PORT || 8080);;
+
+app.listen(port);
 //var io = require('socket.io').listen(server);
 /*
 io.sockets.on('connection', function (socket) {
     console.log("Connected: %s", socket.id);
 });*/
 
-console.log("Polling server is running at " + server);
+console.log("server is running");
